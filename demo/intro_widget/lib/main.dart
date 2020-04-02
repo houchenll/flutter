68d4ccdf99d21b64b6fa66f0691b60ae31910819
houchenll <houchenll@163.com>
2020-04-02 15:42:38 +0800

    Changing widgets in response to input

--- demo/intro_widget/lib/main.dart
+++ demo/intro_widget/lib/main.dart
@@ -90,7 +90,7 @@ class TutorialHome extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          children: <Widget>[Text('Hello World'), MyButton()],
+          children: <Widget>[Text('Hello World'), MyButton(), Counter()],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -119,6 +119,36 @@ class MyButton extends StatelessWidget {
           child: Text('Engage'),
         ),
       ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
+        ),
+        Text('Count: $_counter')
+      ],
     );
   }
 }
