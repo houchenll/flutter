d370eb4193c2f149f21a1317148a50f3e750d22a
houchenll <houchenll@163.com>
2020-04-02 18:22:25 +0800

    add flutter layout demo

---
+++ demo/layout_tutorial/android/app/src/main/kotlin/com/hc/flutter/layoutdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hc.flutter.layoutdemo
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
--- flutter.md
+++ flutter.md
@@ -5,5 +5,14 @@ FlutterGitHubApp
 HistoryOfEverything  
 flutter-go  
 
+## examples
+[flutter-gallery][21]  
+[zero to one with flutter][22]  
+[build layout tutorial][23]  
+
 
 [1]: https://pub.dev/
+
+[21]: https://github.com/flutter/gallery
+[22]: https://medium.com/@mravn/zero-to-one-with-flutter-43b13fd7b354
+[23]: https://flutter.dev/docs/development/ui/layout/tutorial
