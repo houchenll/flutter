6489a15a442e535802f7b6b6be3f3134504d0252
houchenll <houchenll@163.com>
2020-04-08 15:29:28 +0800

    A mix-and-match approach

--- demo/interactivity/lib/main.dart
+++ demo/interactivity/lib/main.dart
@@ -85,7 +85,7 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapBoxB(
+      child: TapBoxC(
         onChanged: _handleTapBoxChanged,
         active: _active,
       ),
@@ -121,6 +121,72 @@ class TapBoxB extends StatelessWidget {
         height: 200.0,
         decoration: BoxDecoration(
             color: active ? Colors.lightGreen[700] : Colors.blue[600]),
+      ),
+    );
+  }
+}
+
+// -------------------- TapBoxC ---------------------------
+
+class TapBoxC extends StatefulWidget {
+  TapBoxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TapBoxCState();
+  }
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  bool _highLight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highLight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highLight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highLight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(
+            widget.active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+            border: _highLight
+                ? Border.all(color: Colors.teal[700], width: 10.0)
+                : null),
       ),
     );
   }
