c39dfa3625e4a4dcaf7f79dd20d3b6de9e8bf720
houchenll <houchenll@163.com>
2020-04-08 17:50:21 +0800

    add: Pass arguments to a named route

--- demo/navigation/lib/main.dart
+++ demo/navigation/lib/main.dart
@@ -7,17 +7,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      // Provide a function to handle named routes. Use this function to
+      // identify the named route being pushed, and create the correct
+      // Screen.
+      onGenerateRoute: (settings) {
+        // If you push the PassArguments route
+        if (settings.name == PassArgumentsScreen.routeName) {
+          // Cast the arguments to the correct type: ScreenArguments.
+          final ScreenArguments args = settings.arguments;
+
+          // Then, extract the required data from the arguments and
+          // pass the data to the correct screen.
+          return MaterialPageRoute(
+            builder: (context) {
+              return PassArgumentsScreen(
+                title: args.title,
+                message: args.message,
+              );
+            },
+          );
+        }
+        // The code only supports PassArgumentsScreen.routeName right now.
+        // Other values need to be implemented if we add them. The assertion
+        // here will help remind us of that higher up in the call stack, since
+        // this assertion would otherwise fire somewhere in the framework.
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
+      },
 //      home: FirstRoute(),    // home can't be used together with initialRoute
-      home: TodoListScreen(
-          todos: List<Todo>.generate(
-              20,
-              (i) => Todo('Todo $i',
-                  'A description for what need to be done for Todo $i'))),
+//      home: TodoListScreen(
+//          todos: List<Todo>.generate(
+//              20,
+//              (i) => Todo('Todo $i',
+//                  'A description for what need to be done for Todo $i'))),
+      home: HomeScreen(),
 //      initialRoute: '/',
-//      routes: {
+      routes: {
 //        '/': (context) => FirstRoute(),
-//        '/second': (context) => SecondRoute(),
-//      },
+        '/second': (context) => SecondRoute(),
+        ExtractArgumentsScreen.routeName: (context) => ExtractArgumentsScreen(),
+      },
     );
   }
 }
@@ -118,4 +147,116 @@ class DescriptionScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // A button that navigates to a named route that. The named route
+            // extracts the arguments by itself.
+            RaisedButton(
+              child: Text("Navigate to screen that extracts arguments"),
+              onPressed: () {
+                // When the user taps the button, navigate to a named route
+                // and provide the arguments as an optional parameter.
+                Navigator.pushNamed(
+                  context,
+                  ExtractArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Extract Arguments Screen',
+                    'This message is extracted in the build method.',
+                  ),
+                );
+              },
+            ),
+            // A button that navigates to a named route. For this route, extract
+            // the arguments in the onGenerateRoute function and pass them
+            // to the screen.
+            RaisedButton(
+              child: Text("Navigate to a named that accepts arguments"),
+              onPressed: () {
+                // When the user taps the button, navigate to a named route
+                // and provide the arguments as an optional parameter.
+                Navigator.pushNamed(
+                  context,
+                  PassArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Accept Arguments Screen',
+                    'This message is extracted in the onGenerateRoute function.',
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// A widget that extracts the necessary arguments from the ModalRoute.
+class ExtractArgumentsScreen extends StatelessWidget {
+  static const String routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute settings and cast
+    // them as ScreenArguments.
+    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+      ),
+      body: Center(
+        child: Text(args.message),
+      ),
+    );
+  }
+}
+
+// A Widget that accepts the necessary arguments via the constructor.
+class PassArgumentsScreen extends StatelessWidget {
+  static const routeName = '/passArguments';
+
+  final String title;
+  final String message;
+
+  // This Widget accepts the arguments as constructor parameters. It does not
+  // extract the arguments from the ModalRoute.
+  //
+  // The arguments are extracted by the onGenerateRoute function provided to the
+  // MaterialApp widget.
+  const PassArgumentsScreen({
+    Key key,
+    @required this.title,
+    @required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
+
+class ScreenArguments {
+  final String title;
+  final String message;
+
+  ScreenArguments(this.title, this.message);
 }
