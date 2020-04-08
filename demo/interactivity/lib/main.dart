5e1a5b1daf38fc68ddad9663f2ceed6a5abd3f31
houchenll <houchenll@163.com>
2020-04-08 15:09:58 +0800

    Parent widget manage the state for TapBoxB

--- demo/interactivity/lib/main.dart
+++ demo/interactivity/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Interactivity'),
         ),
         body: Center(
-          child: TapBoxA(),
+          child: ParentWidget(),
         ),
       ),
     );
@@ -57,6 +57,70 @@ class _TapBoxAState extends State<TapBoxA> {
         height: 200.0,
         decoration: BoxDecoration(
             color: _active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+  }
+}
+
+// Parent widget manage the state for TapBoxB
+
+// -------------------- ParentWidget ---------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ParentWidgetState();
+  }
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapBoxB(
+        onChanged: _handleTapBoxChanged,
+        active: _active,
+      ),
+    );
+  }
+}
+
+// -------------------- TapBoxB ---------------------------
+
+class TapBoxB extends StatelessWidget {
+  TapBoxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'InActive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.blue[600]),
       ),
     );
   }
