61e48a4baec3b465e53eb1c3baee0dea40924f50
houchenll <houchenll@163.com>
2020-04-08 17:13:51 +0800

    Send data to a new screen

--- demo/navigation/lib/main.dart
+++ demo/navigation/lib/main.dart
@@ -8,11 +8,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
 //      home: FirstRoute(),    // home can't be used together with initialRoute
-      initialRoute: '/',
-      routes: {
-        '/': (context) => FirstRoute(),
-        '/second': (context) => SecondRoute(),
-      },
+      home: TodoListScreen(
+          todos: List<Todo>.generate(
+              20,
+              (i) => Todo('Todo $i',
+                  'A description for what need to be done for Todo $i'))),
+//      initialRoute: '/',
+//      routes: {
+//        '/': (context) => FirstRoute(),
+//        '/second': (context) => SecondRoute(),
+//      },
     );
   }
 }
@@ -54,6 +59,62 @@ class SecondRoute extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
+      ),
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
+class TodoListScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodoListScreen({Key key, @required this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(todos[index].title),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DescriptionScreen(),
+                      settings: RouteSettings(
+                        arguments: todos[index],
+                      ),
+                    ));
+              },
+            );
+          }),
+    );
+  }
+}
+
+class DescriptionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Todo todo = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
       ),
     );
   }
