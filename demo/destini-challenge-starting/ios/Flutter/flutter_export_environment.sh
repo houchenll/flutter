b2123f47584cb25cb0543cab133c81a448d035a6
houchenll <houchenll@163.com>
2020-04-16 16:05:05 +0800

    complete destini project

--- demo/destini-challenge-starting/ios/Flutter/flutter_export_environment.sh
+++ demo/destini-challenge-starting/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ll/env/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/ll/workspace/flutter/demo/destini-challenge-starting"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/ll/workspace/flutter/demo/destini-challenge-starting/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/ll/env/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
